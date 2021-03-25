0829bad495b16b3020488f7c4fa1b9837f57916b
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-03-25 17:54:38 +0900

    [feat] add card and edit form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,12 +1,47 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router";
-import Editor from "../editor/editor";
-import Footer from "../footer/footer";
-import Header from "../header/header";
-import Preview from "../preview/preview";
-import styles from "./maker.module.css";
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+import Editor from '../editor/editor';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import Preview from '../preview/preview';
+import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Gyu',
+      company: 'Naver',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: 'gyu@gmail.com',
+      message: 'go for it',
+      fileName: 'gyu',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Gyu2',
+      company: 'Naver',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'gyu@gmail.com',
+      message: 'go for it',
+      fileName: 'gyu',
+      fileURL: 'ellie',
+    },
+    {
+      id: '3',
+      name: 'Gyu3',
+      company: 'Naver',
+      theme: 'colorful',
+      title: 'Software Engineer',
+      email: 'gyu@gmail.com',
+      message: 'go for it',
+      fileName: 'gyu',
+      fileURL: null,
+    },
+  ]);
   const history = useHistory();
 
   const onLogout = () => {
@@ -15,7 +50,7 @@ const Maker = ({ authService }) => {
 
   useEffect(() => {
     authService.onAuthChange((user) => {
-      !user && history.push("/");
+      !user && history.push('/');
     });
   });
 
@@ -23,8 +58,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
