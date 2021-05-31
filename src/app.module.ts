bd692f9708a2315a06dcc3db432ceb65495dcf75
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-06-01 00:25:52 +0900

    [refactor] config 로 환경 변수 관리
    - config 와 schema validator joi 를 통해 .env 파일에서 환경 변수 관리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
+    }),
+    UsersModule,
+    AuthModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
