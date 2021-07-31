7a6d4cced3529db85d3dd3a7ba8214167196c318
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-08-01 06:01:13 +0900

    [feat] redux 적용

---
+++ blog-frontend/src/modules/auth.js
@@ -0,0 +1,20 @@
+import { createAction, handleActions } from 'redux-actions';
+
+// 액션 타입
+const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+
+// 액션 생성 함수
+export const sampleAction = createAction(SAMPLE_ACTION);
+
+// 초기 값
+const initialState = {};
+
+// 리듀서
+const auth = handleActions(
+  {
+    [SAMPLE_ACTION]: (state, action) => state,
+  },
+  initialState,
+);
+
+export default auth;
