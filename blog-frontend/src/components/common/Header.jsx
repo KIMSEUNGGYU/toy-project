4899aac522525dc569ca573b377225670bfaa518
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-08-02 20:30:34 +0900

    [feat] 헤더 로그인 기능 연동

--- blog-frontend/src/components/common/Header.jsx
+++ blog-frontend/src/components/common/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 import Responsive from './Responsive';
 import Button from './Button';
 
@@ -36,15 +38,29 @@ const Spacer = styled.div`
   height: 4rem;
 `;
 
-const Header = () => {
+const UserInfo = styled.div`
+  font-weight: 800;
+  margin-right: 1rem;
+`;
+
+const Header = ({ user }) => {
   return (
     <>
       <HeaderBlock>
         <Wrapper>
-          <div className="logo">REACTERS</div>
-          <div className="right">
-            <Button to="/login">로그인</Button>
-          </div>
+          <Link to="/" className="logo">
+            REACTERS
+          </Link>
+          {user ? (
+            <div className="right">
+              <UserInfo>{user.username}</UserInfo>
+              <Button>로그아웃</Button>
+            </div>
+          ) : (
+            <div className="right">
+              <Button to="/login">로그인</Button>
+            </div>
+          )}
         </Wrapper>
       </HeaderBlock>
       <Spacer />
