fc1a28949f80f5ecb22a554c45db93471c0e9d69
kimseunggyu <dev.seunggyu@gmail.com>
2022-08-11 23:05:47 +0900

    [feat] 3-2 워크스페이스 만들기(중첩 라우터)

--- src/layouts/Workspace/index.tsx
+++ src/layouts/Workspace/index.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useCallback } from 'react';
-import { Redirect } from 'react-router';
+import React, { useCallback, VFC } from 'react';
+import { Redirect, Route, Switch } from 'react-router';
 import useSWR from 'swr';
 import axios from 'axios';
 import gravatar from 'gravatar';
+import loadable from '@loadable/component';
 
 import fetcher from '@utils/fetcher';
 import {
@@ -15,11 +16,16 @@ import {
   Workspaces,
   WorkspaceWrapper,
 } from '@layouts/Workspace/styles';
+// import Channel from '@pages/Channel';
+// import DirectMessage from '@pages/DirectMessage';
+
+const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 // [💡 GYU]
 // FC 타입은 children 사용하는 컴포넌트
 // VFC 타입은 children 사용하지 않는 컴포넌트
-const Workspace: FC = ({ children }) => {
+const Workspace: VFC = () => {
   const { data, error, revalidate, mutate } = useSWR('/api/users', fetcher, {
     // 💡 100초 동안 캐시, 그래서 다른 곳에서 해당 작업이 있더라도 캐시 기간동안은 캐시값을 읽어 서버에 무리를 최소화
     // 실시간 데이터시,  dedupingInterval 의 시간을 줄이면 됨!
@@ -53,9 +59,13 @@ const Workspace: FC = ({ children }) => {
           <WorkspaceName>Sleact</WorkspaceName>
           <MenuScroll>menu scroll</MenuScroll>
         </Channels>
-        <Chats>Chatgs</Chats>
+        <Chats>
+          <Switch>
+            <Route path="/workspace/channel" component={Channel} />
+            <Route path="/workspace/dm" component={DirectMessage} />
+          </Switch>
+        </Chats>
       </WorkspaceWrapper>
-      {children}
     </>
   );
 };
