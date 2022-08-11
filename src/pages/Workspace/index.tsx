cfb883c863505af1fd597411b9098202cbe3b4c8
kimseunggyu <dev.seunggyu@gmail.com>
2022-08-11 22:29:07 +0900

    [feat] 2-8 swr 활용법 (optimistic ui)

--- src/pages/Workspace/index.tsx
+++ src/pages/Workspace/index.tsx
@@ -9,13 +9,17 @@ import fetcher from '@utils/fetcher';
 // FC 타입은 children 사용하는 컴포넌트
 // VFC 타입은 children 사용하지 않는 컴포넌트
 const Workspace: FC = ({ children }) => {
-  const { data, error, revalidate } = useSWR('/api/users', fetcher, { dedupingInterval: 100000 });
+  const { data, error, revalidate, mutate } = useSWR('/api/users', fetcher, {
+    // 💡 100초 동안 캐시, 그래서 다른 곳에서 해당 작업이 있더라도 캐시 기간동안은 캐시값을 읽어 서버에 무리를 최소화
+    // 실시간 데이터시,  dedupingInterval 의 시간을 줄이면 됨!
+    dedupingInterval: 100000,
+  });
 
   const onLogout = useCallback(() => {
     axios //
       .post('http://localhost:3095/api/users/logout', null, { withCredentials: true })
       .then(() => {
-        revalidate();
+        mutate(false);
       });
   }, []);
 
