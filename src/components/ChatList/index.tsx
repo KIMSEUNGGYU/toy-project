91ad8160bb9c2246cabae4d7836ba0c78d4f2973
kimseunggyu <dev.seunggyu@gmail.com>
2022-08-13 01:56:45 +0900

    [feat] 5-8 리버스 인피니트 스크롤링(useSWRInfinite)

--- src/components/ChatList/index.tsx
+++ src/components/ChatList/index.tsx
@@ -1,21 +1,42 @@
-import React, { useCallback, useRef, VFC } from 'react';
+import React, { useCallback, VFC, forwardRef } from 'react';
 
 import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
-import { IDM } from '@typings/db';
+import { IChat, IDM } from '@typings/db';
 import Chat from '@components/Chat';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
-  chatSections: { [key: string]: IDM[] };
+  // scrollbarRef: RefObject<Scrollbars>;
+  isReachingEnd?: boolean;
+  isEmpty: boolean;
+  chatSections: { [key: string]: (IDM | IChat)[] };
+  setSize: (f: (size: number) => number) => Promise<(IDM | IChat)[][] | undefined>;
 }
 
-const ChatList: VFC<Props> = ({ chatSections }) => {
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(() => {}, []);
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isEmpty, isReachingEnd }, ref) => {
+  const onScroll = useCallback(
+    (values) => {
+      // values.scrollTop === 0 (가장위)
+      if (values.scrollTop === 0 && !isReachingEnd && !isEmpty) {
+        setSize((size) => size + 1).then(() => {
+          // 위치 수정
+          // scrollbarRef.current?.scrollTop(scrollbarRef.current?.getScrollHeight() - values.scrollHeight);
+        });
+      }
+    },
+    [setSize, isReachingEnd, isEmpty],
+    // [setSize, scrollbarRef, isReachingEnd, isEmpty],
+  );
+
+  // const onScroll = useCallback((values) => {
+  //   if (values.scrollTop === 0) {
+  //     console.log('가장 위');
+  //   }
+  // }, []);
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+      <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
         {Object.entries(chatSections).map(([date, chats]) => {
           return (
             <Section className={`section-${date}`} key={date}>
@@ -31,6 +52,6 @@ const ChatList: VFC<Props> = ({ chatSections }) => {
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 export default ChatList;
--- src/pages/DirectMessage/index.tsx
+++ src/pages/DirectMessage/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react';
-import useSWR from 'swr';
+import React, { useCallback, useRef } from 'react';
+import useSWR, { useSWRInfinite } from 'swr';
 import gravatar from 'gravatar';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import { Container, Header } from './styles';
 import fetcher from '@utils/fetcher';
@@ -12,18 +13,29 @@ import axios from 'axios';
 import { IDM } from '@typings/db';
 import makeSection from '@utils/makeSection';
 
+const PAGE_SIZE = 20;
+
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
 
   const { data: myData } = useSWR('/api/users', fetcher);
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const [chat, onChangeChat, setChat] = useInput('');
+  const scrollbarRef = useRef<Scrollbars>(null);
 
   const {
     data: chatData,
     mutate: mutateChat,
     revalidate,
-  } = useSWR<IDM[]>(`/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher);
+    setSize, // page 수 변경
+  } = useSWRInfinite<IDM[]>(
+    (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
+    fetcher,
+  );
+
+  // [💡 GYU] infinite scroll 할 때 아래 2개 같이 사용하는게 좋음
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < PAGE_SIZE); // 데이터 다 가져왔는지
 
   const onSubmitForm = useCallback(
     (e) => {
@@ -50,7 +62,7 @@ const DirectMessage = () => {
 
   if (!userData || !myData) return null;
 
-  const chatSections = makeSection(chatData ? [...chatData].reverse() : []);
+  const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
 
   return (
     <Container
@@ -60,14 +72,15 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      {/* <ChatList
-        scrollbarRef={scrollbarRef}
+      <ChatList
+        // scrollbarRef={scrollbarRef}
         isReachingEnd={isReachingEnd}
         isEmpty={isEmpty}
         chatSections={chatSections}
+        ref={scrollbarRef}
         setSize={setSize}
-      /> */}
-      <ChatList chatSections={chatSections} />
+      />
+      {/* <ChatList chatSections={chatSections}  /> */}
       <ChatBox
         chat={chat}
         onChangeChat={onChangeChat}
