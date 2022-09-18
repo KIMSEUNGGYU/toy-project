b2b5b3e02c9a4cde87db5b841940246b411245c6
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2022-09-19 06:50:24 +0900

    [01] 인프런(제로초) - sleact 강의 기반 slack 클론 코딩 (#3)
    * Initial commit
    
    * [init] 초기 셋팅
    
    * [init] 폴더 구조 변경
    
    * [init] 초기 환경 설정
    
    * [feat] 2-1 회원가입 페이지 만들기
    
    * [feat] 2-2 커스텀 훅 만들기
    - useInput
    
    * [feat] 2-3 axios로 요청 보내기와 CORS, proxy
    
    * [feat] 2-4 로그인 페이지 만들기
    
    * [feat] 2-5 swr 사용하기(쿠키 공유하기)
    
    * [feat] 2-6 swr 설정 살펴보기
    
    * [feat] 2-7 워크스페이스 만들기 + 로그아웃하기
    
    * [feat] 2-8 swr 활용법 (optimistic ui)
    
    * [feat] 3-1 워크스페이스 만들기(gravatar)
    
    * [feat] 3-2 워크스페이스 만들기(중첩 라우터)
    
    * [feat] 3-3 메뉴 만들기
    
    * [feat] 3-4 모달 만들기
    
    * [feat] 3-5 채널 만드는 모달
    
    * [feat] 3-6 라우터 주소 설계(라우트 파라미터)
    
    * [feat] 3-7 사용자 초대 모달 만들기
    
    * [feat] 4-1 DM 목록 만들기
    
    * [feat] 4-2 채널 목록 만들기
    
    * [feat] 4-3 DM 페이지 만들기
    
    * [feat] 4-4 챗박스 만들기
    
    * [feat] 4-5 채팅 보내기
    
    * [feat] 5-1 socket.io 전용 훅스 만들기
    
    * [feat] 5-2 socket.io 이벤트 연결하기
    
    * [feat] 5-3 DM 내용 표시하기
    
    * [feat] 5-4 커스텀 스크롤바와 dayjs
    
    * [feat] 5-5 멘션 기능 만들기
    
    * [feat] 5-6 정규표현식으로 문자열 변환하기
    
    * [feat] 5-7 날짜별로 묶어주기(position: sticky)
    
    * [feat] 5-8 리버스 인피니트 스크롤링(useSWRInfinite)
    
    * [feat] 5-9 스크롤바 조절하기
    
    * [feat] 5-10 DM 채팅하기
    
    * [feat] 6-1~2 채널 만들기 및 타입 점검하기(타입 가드)
    
    * [feat] 6-3 배포 준비하기
    
    * [feat] 7-1 이미지 드래그 업로드하기
    
    * Initial commit
    
    * [docs] 레포 설명 글

---
+++ 01-sleact/01-sleact/src/components/CreateChannelModal/index.tsx
@@ -0,0 +1,71 @@
+import React, { useCallback, VFC } from 'react';
+import { useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+
+import Modal from '@components/Modal';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import useInput from '@hooks/useInput';
+import useSWR from 'swr';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  setShowCloaseModal: (flag: boolean) => void;
+}
+
+const CreateChannelModal: VFC<Props> = ({ show, onCloseModal, setShowCloaseModal }) => {
+  const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
+
+  const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
+
+  const { data: userData } = useSWR<IUser | false>('/api/users', fetcher, {
+    dedupingInterval: 100000,
+  });
+
+  const { revalidate: revalidateChannel } = useSWR<IChannel[]>(
+    userData ? `/api/workspaces/${workspace}/channels` : null,
+    fetcher,
+  );
+
+  const onCreateChannel = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      axios
+        .post(
+          `/api/workspaces/${workspace}/channels`,
+          {
+            name: newChannel,
+          },
+          { withCredentials: true },
+        )
+        .then(() => {
+          revalidateChannel();
+          setShowCloaseModal(false);
+          setNewChannel('');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newChannel],
+  );
+
+  return (
+    <Modal show={show} onCloseModal={onCloseModal}>
+      <form onSubmit={onCreateChannel}>
+        <Label id="channel-label">
+          <span>채널</span>
+          <Input id="channel-label" value={newChannel} onChange={onChangeNewChannel} />
+        </Label>
+        <Button type="submit">생성하기</Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default CreateChannelModal;
