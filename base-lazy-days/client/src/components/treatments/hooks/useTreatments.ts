683e94c85903059c76b2f8c355211fc29337585b
kimseunggyu <dev.seunggyu@gmail.com>
2022-09-23 20:40:14 +0900

    50. 리페치(Re-fetch) 옵션 업데이트

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -13,12 +13,21 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 1000 * 60 * 10, // 10m
+    cacheTime: 1000 * 60 * 15, // 15m
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
   return data;
 }
 
 export function usePrefetchTreatments(): void {
   const queryClient = useQueryClient();
 
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 1000 * 60 * 10, // 10m
+    cacheTime: 1000 * 60 * 15, // 15m
+  });
 }
