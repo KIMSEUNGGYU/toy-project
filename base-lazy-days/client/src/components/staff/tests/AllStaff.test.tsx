8aef2cfd08e6f1abd923e83f5828ad7f2983d156
kimseunggyu <dev.seunggyu@gmail.com>
2022-09-26 08:29:31 +0900

    80. 코드 퀴즈! 직원 쿼리 에러

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,8 +1,11 @@
-import { screen } from '@testing-library/react';
+/* eslint-disable no-console */
+import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { QueryClientProvider, setLogger } from 'react-query';
+import { generateQueryClient } from 'react-query/queryClient';
 
-// import { rest } from 'msw';
+import { server } from '../../../mocks/server';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { server } from '../../../mocks/server';
 import { renderWithQueryClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
@@ -17,9 +20,35 @@ test('renders response from query', async () => {
 
 test('handles query error', async () => {
   // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+  server.resetHandlers(
+    rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+  );
+
+  // supress errors
+  setLogger({
+    log: console.log,
+    warn: console.warn,
+    error: () => {
+      // shallow errors without pariting out
+    },
+  });
+
+  // set up query client with retries set to false
+  const queryClient = generateQueryClient();
+  const options = queryClient.getDefaultOptions();
+  options.queries = { ...options.queries, retry: false };
+  queryClient.setDefaultOptions(options);
+
+  // render wrapped with provider
+  render(
+    <QueryClientProvider client={queryClient}>
+      <AllStaff />
+    </QueryClientProvider>,
+  );
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('Request failed with status code 500');
 });
