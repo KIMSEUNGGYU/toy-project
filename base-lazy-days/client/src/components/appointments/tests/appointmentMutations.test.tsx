e7a3324c45a8b8c86caf6695dac8f5d45a35eb79
kimseunggyu <dev.seunggyu@gmail.com>
2022-09-26 08:38:30 +0900

    81. 변이(Mutation) 테스트하기

--- base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
@@ -1,37 +1,44 @@
 import {
   fireEvent,
   screen,
-  waitForElementToBeRemoved,
+  waitForElementToBeRemoved, // 요소가 제거될때까지 기다림 (Toast 가 사라질때까지 )
 } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom'; // react-router 를 테스트하기 위해 MemoryRouter 사용
 
 import { mockUser } from '../../../mocks/mockData';
-// import { renderWithQueryClient } from '../../../test-utils';
+import { renderWithQueryClient } from '../../../test-utils';
 import { Calendar } from '../Calendar';
 
 // mocking useUser to mimic a logged-in user
+// useUser 를 mocking해서 useUser 를 쓸모없게 만듦 => 사실상 user 데이터만 들어있게 함.
 jest.mock('../../user/hooks/useUser', () => ({
   __esModule: true,
   useUser: () => ({ user: mockUser }),
 }));
 
 test('Reserve appointment', async () => {
+  renderWithQueryClient(
+    <MemoryRouter>
+      <Calendar />
+    </MemoryRouter>,
+  );
+
   // find all the appointments
-  // const appointments = await screen.findAllByRole('button', {
-  //   name: /\d\d? [ap]m\s+(scrub|facial|massage)/i,
-  // });
-  //
-  // // click on the first one to reserve
-  // fireEvent.click(appointments[0]);
-  //
-  // // check for the toast alert
-  // const alertToast = await screen.findByRole('alert');
-  // expect(alertToast).toHaveTextContent('reserve');
-  //
-  // // close alert to keep state clean and wait for it to disappear
-  // const alertCloseButton = screen.getByRole('button', { name: 'Close' });
-  // alertCloseButton.click();
-  // await waitForElementToBeRemoved(alertToast);
+  const appointments = await screen.findAllByRole('button', {
+    name: /\d\d? [ap]m\s+(scrub|facial|massage)/i,
+  });
+
+  // click on the first one to reserve
+  fireEvent.click(appointments[0]);
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('You have reseverd the appointment!');
+
+  // close alert to keep state clean and wait for it to disappear
+  const alertCloseButton = screen.getByRole('button', { name: 'Close' });
+  alertCloseButton.click();
+  await waitForElementToBeRemoved(alertToast);
 });
 
 test('Cancel appointment', async () => {
