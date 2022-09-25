88a1f68852ab9f48427b93449e90dca9f5072401
kimseunggyu <dev.seunggyu@gmail.com>
2022-09-26 08:42:26 +0900

    82. 코드 퀴즈! 예약 취소 변이(Mutation) 테스트하기

--- base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
@@ -42,9 +42,25 @@ test('Reserve appointment', async () => {
 });
 
 test('Cancel appointment', async () => {
-  // your test here
-  //
-  // const cancelButtons = await screen.findAllByRole('button', {
-  //   name: /cancel appointment/i,
-  //  });
+  renderWithQueryClient(
+    <MemoryRouter>
+      <Calendar />
+    </MemoryRouter>,
+  );
+
+  const cancelButtons = await screen.findAllByRole('button', {
+    name: /cancel appointment/i,
+  });
+
+  // click on the first one to cacnel
+  fireEvent.click(cancelButtons[0]);
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('cancel');
+
+  // close alert to keep state clean and wait for it to disappear
+  const alertCloseButton = screen.getByRole('button', { name: 'Close' });
+  alertCloseButton.click();
+  await waitForElementToBeRemoved(alertToast);
 });
