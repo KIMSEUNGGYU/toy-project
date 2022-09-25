d7a988c9bc3e2b67f7350edc548d8450cac2f8b6
kimseunggyu <dev.seunggyu@gmail.com>
2022-09-26 04:03:54 +0900

    68. 코드 퀴즈! 변이(Mutation)로 예약 취소하기

--- base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
@@ -1,22 +1,37 @@
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
+
 import { Appointment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when server call is needed
-// async function removeAppointmentUser(appointment: Appointment): Promise<void> {
-//   const patchData = [{ op: 'remove', path: '/userId' }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function removeAppointmentUser(appointment: Appointment): Promise<void> {
+  const patchData = [{ op: 'remove', path: '/userId' }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
 // TODO: update return type
-export function useCancelAppointment(): (appointment: Appointment) => void {
+export function useCancelAppointment(): UseMutateFunction<
+  void, // 쿼리 함수?(removeAppointmentUser) 의 반환값
+  unknown, // 오류 유형
+  Appointment, // mutate 함수에 대한 인수는 Appointment 유형
+  unknown // onMutate 컨텍스트는 unknown // 콘텍스트 없음, onMutate 함수 실행 X (??)
+> {
   const toast = useCustomToast();
+  const queryClient = useQueryClient();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation(removeAppointmentUser, {
+    onSuccess: () => {
+      queryClient.invalidateQueries([queryKeys.appointments]);
+      toast({
+        title: 'You have canceld the appoinment!',
+        status: 'warning',
+      });
+    },
+  });
+
+  return mutate;
 }
